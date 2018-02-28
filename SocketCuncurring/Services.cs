932c5ac938d6a60c089deb284f6d9f0fe43110be
wandriey <jona434g@edu.easj.dk>
2018-02-28 10:15:28 +0100

    Initial commit

---
+++ SocketCuncurring/Services.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketCuncurring
+{
+    class Services
+    {
+
+        private TcpClient _connectionSocket;
+        public Services(TcpClient connection)
+        {
+            _connectionSocket = connection;
+        }
+
+        public void doIt()
+        {
+            Stream ns = _connectionSocket.GetStream();
+            //Stream ns = new NetworkStream(connectionSocket);
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = sr.ReadLine();
+            string answer = "";
+            while (message != null && message != "")
+            {
+                Console.WriteLine("Client: " + message);
+                answer = message.ToUpper();
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+            }
+
+            ns.Close();
+            _connectionSocket.Close();
+        }
+    }
+}
---
+++ SocketCuncurring/SocketServer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SocketCuncurring
+{
+    class SocketServer
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("192.168.6.76");
+            TcpListener serverSocket = new TcpListener(ip, 6789);
+            serverSocket.Start();
+
+            while (true)
+            {
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                //Socket connectionSocket = serverSocket.AcceptSocket();
+                Console.WriteLine("Server activated");
+
+                Services services = new Services(connectionSocket);
+               // services.doIt();                                      //Vi skal ikke kalde doit metoden selv, da thread tager det som parameter
+
+
+                Thread thread = new Thread(services.doIt);              //Hver gang man løber en while loopen igennem, så laves der en ny tråd.
+                thread.Start(); 
+
+            }
+
+            serverSocket.Stop();
+        }
+    }
+}
